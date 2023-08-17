e10445880a60ec7283bf53839d53b73e5c46fe08
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-08-17 16:55:55 -0500

    commit albums_seeder.sql, wiki album info complete

---
+++ albums_seeder.sql
@@ -0,0 +1,91 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Thriller', 1982, 51200000, 'Pop, post-disco, funk, rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('AC/DC', 'Back in Black', 1980, 30100000, 'Hard Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Whitney Houston', 'The Bodyguard', 1992, 28700000, 'R&B, soul, pop, soundtrack');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', 1973, 28700000, 'Progressive Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 41200000, 'Country rock, soft rock, folk rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('MeatLoaf', 'Bat Out of Hell', 1977, 31800000, 'Hard rock, glam rock, progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Eagles', 'Hotel California', 1976, 31800000, 'Soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Shania Twain', 'Come On Over', 1997, 30400000, 'Country, pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Fleetwood Mac', 'Rumours', 1977, 30300000, 'Soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bee Gees', 'Saturday Night Fever', 1977, 22100000, 'Disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', 'Abbey Road', 1969, 16900000, 'Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Dangerous', 1991, 17000000, 'New jack swing, R&B and pop');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 1967, 18300000, 'Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Pink Floyd', 'The Wall', 1979, 18900000, 'Progressive rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Celine Dion', 'Lets Talk About Love', 1997, 20500000, 'Pop, soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Madonna', 'The Immaculate Collection', 1990, 20800000, 'Pop Dance');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Santana', 'Supernatural', 1999, 20800000, 'Latin rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('ABBA', 'Gold: Greatest Hits', 1992, 21600000, 'Pop, disco');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Dire Straits', 'Brothers in Arms', 1985, 30000000, 'Roots rocks, blue rock, soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Celine Dion', 'Falling into You', 1996, 21100000, 'Pop, soft rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson', 'Bad', 1987, 21200000, 'Pop, rhythm and blues, funk and rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 1984, 22000000, 'Heartland rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Metallica', 'Metallica', 1991, 22700000, 'Heavy Metal');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Guns N Roses', 'Appetite for Destruction', 1987, 22800000, 'Hark rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Bob Marley and the Wailers', 'Legend: The Best of Bob Marley & The Wailers', 1984, 22900000, 'Reggae');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('The Beatles', '1', 2000, 23200000, 'Rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Various Artists', 'Dirty Dancing', 1987, 24100000, 'Pop, rock, R&B');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Alanis Morissette', 'Jagged Little Pill', 1995, 25400000, 'Alternative rock');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Adele', '21', 2011, 27100000, 'Pop, soul');
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 1971, 30400000, 'Hard rock, heavy metal, folk rock');
