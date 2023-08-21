549513495e1420470950ed9794729d8f50cb0277
Nicholas Rowley <nicholas.ryan.rowley@gmail.com>
2023-08-21 09:13:34 -0500

    test token

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,6 +3,7 @@
 #List the first 10 distinct last name sorted in descending order. Your result should look like this:
 #Create a query to get the top 5 salaries and display just the employees number from the salaries table. The employee number results should be:
 #Try to think of your results as batches, sets, or pages. The first five results are your first page. The five after that would be your second page, etc. Update the previous query to find the tenth page of results. The employee number results should be:
+#Test
 
 USE employees;
 
